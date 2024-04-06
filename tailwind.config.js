6e6f27c592e1010c0c832d8da5ffd8987645526f
linhnguyen2803 <nguyenlinh8203@gmail.com>
2024-04-06 09:53:13 +0700

    Background display settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,9 @@ module.exports = {
         backgr: "url('/backgr2.jpg')",
         backgr1: "url('/anhnnen01.jpg')",
         backgr2: "url('/backgr2.jpg')",
-        
+        backgr3: "url('/Backr001.jpg')",
+        backgr4: "url('/Backr002.jpg')",
+        backgr5: "url('/Back003.jpg')",
       },
     },
   },
